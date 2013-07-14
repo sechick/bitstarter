775053c335898ff8d78f8cb9e0cf31047c0a61ca
sechick <sechick@alumni.stanford.edu>
2013-07-14 08:52:09 +0000

    try 3

--- web.js
+++ web.js
@@ -3,12 +3,18 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var infile = "index.html";
+var infile = "./index.html";
 
 app.get('/', function(request, response) {
-  var mybuffer = fs.readFileSync(infile);
-  response.send(mybuffer.toString);
-});
+  var mybuffer;
+  fs.readFileSync(infile, function read(err, data) {
+    if (err) {
+  console.log("error with file read " + err);
+      throw err;
+    }
+    mybuffer = data;
+  response.send(mybuffer.toString('utf-8'));
+}));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
