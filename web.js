89d9d30e02e310c8011e039888ebd3f62291b27d
sechick <sechick@alumni.stanford.edu>
2013-07-14 09:04:23 +0000

    another edit

--- web.js
+++ web.js
@@ -2,7 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
+
+  var contents = fs.readFileSync('index.html','utf8',function(err,data) {
+  if (err) {
+    response.send('unable to load the file ' + err);
+  } else {
+    response.send(data.toString('utf8'));
+  }
+  }
   response.send('Hello World 2!');
 });
 
